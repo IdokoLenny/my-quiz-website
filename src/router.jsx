bc51d1855ed76cdb4f57edf841d25f5c253e72fe
idokolenny <idokoleonardz@gmail.com>
2024-11-22 17:46:10 +0100

    feat: update the router.jsx file

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,15 @@
-import { createBrowserRouter} from "react-router-dom"
-import QuestionPage from "./routes/QuestionPage.jsx"
-import ResultPage from "./routes/ResultPage.jsx"
-import App from "./App.jsx"
+import { createBrowserRouter } from "react-router-dom";
+import QuestionPage from "./routes/QuestionPage.jsx";
+import ResultPage from "./routes/ResultPage.jsx";
+import App from "./App.jsx";
 
-const basename = "my-quiz-website"
+const basename = "/my-quiz-website";
 
-export const router = createBrowserRouter([
-    {path: "/", element: <App />},
-    {path: "/questions", element: <QuestionPage />},
-    {path: "/result", element: <ResultPage />}
-  ],{basename})
-  
+export const router = createBrowserRouter(
+  [
+    { path: "/", element: <App /> },
+    { path: "/questions", element: <QuestionPage /> },
+    { path: "/result", element: <ResultPage /> },
+  ],
+  { basename }
+);
