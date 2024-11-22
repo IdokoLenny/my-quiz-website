1f1be8a9698d6cb0ce1431275abc1e1d0c21ceea
idokolenny <idokoleonardz@gmail.com>
2024-11-22 18:31:30 +0100

    fix: delete a tag that was never used for easy  deployment

--- src/router.jsx
+++ src/router.jsx
@@ -3,13 +3,10 @@ import QuestionPage from "./routes/QuestionPage.jsx";
 import ResultPage from "./routes/ResultPage.jsx";
 import App from "./App.jsx";
 
-const basename = "/my-quiz-website";
-
 export const router = createBrowserRouter(
   [
     { path: "/", element: <App /> },
     { path: "/questions", element: <QuestionPage /> },
     { path: "/result", element: <ResultPage /> },
-  ],
-  { basename }
+  ]
 );
