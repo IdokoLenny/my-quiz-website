3b3f3c3086735a0026ccbffd4c2f7c7b50b659e6
idokolenny <idokoleonardz@gmail.com>
2024-11-23 09:31:46 +0100

    fix: update vite.config.js file

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basename={"/my-quiz-website/"} />
   </StrictMode>,
 )
